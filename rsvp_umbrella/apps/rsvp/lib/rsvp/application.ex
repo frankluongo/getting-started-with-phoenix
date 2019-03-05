20b72bbf388b3ebc29ea2928a8da8d4afde7b0bd
Frank Luongo <frankluongo92@gmail.com>
2019-03-05 17:54:57 -0500

    S102 Installing Ecto

--- rsvp_umbrella/apps/rsvp/lib/rsvp/application.ex
+++ rsvp_umbrella/apps/rsvp/lib/rsvp/application.ex
@@ -10,6 +10,8 @@ defmodule Rsvp.Application do
     children = [
       # Starts a worker by calling: Rsvp.Worker.start_link(arg)
       # {Rsvp.Worker, arg}
+      # {Rsvp.Worker}
+      Rsvp.Repo
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
