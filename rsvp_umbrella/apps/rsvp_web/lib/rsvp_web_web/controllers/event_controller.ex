483522c6b48841ae67b86013c102f7259da70b76
Frank Luongo <frankluongo92@gmail.com>
2019-03-06 18:41:10 -0500

    S204 Creating The Show Function

--- rsvp_umbrella/apps/rsvp_web/lib/rsvp_web_web/controllers/event_controller.ex
+++ rsvp_umbrella/apps/rsvp_web/lib/rsvp_web_web/controllers/event_controller.ex
@@ -1,4 +1,13 @@
 defmodule RsvpWebWeb.EventController do
   # Phoenix gives you this :controller option to help you build controllers
   use RsvpWebWeb, :controller
+
+  # All of these functions require two parameters, the connection and params sent along from the router
+  def show(conn, params) do
+    # Render takes three arguments, the conn, the template, and the data to be sent along
+    render conn, "details.html", "Events #{params["id"]}"
+
+    # Text is a function. It renders out the text supplied as the second argument
+    # text conn, "Events #{params["id"]}"
+  end
 end
