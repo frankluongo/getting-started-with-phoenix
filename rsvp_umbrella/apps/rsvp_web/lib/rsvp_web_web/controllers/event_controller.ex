6e912d67f4af17310dbe3ca8aad656bc50790a19
Frank Luongo <frankluongo92@gmail.com>
2019-03-06 19:02:46 -0500

    S206 Supplying Data to a View

--- rsvp_umbrella/apps/rsvp_web/lib/rsvp_web_web/controllers/event_controller.ex
+++ rsvp_umbrella/apps/rsvp_web/lib/rsvp_web_web/controllers/event_controller.ex
@@ -3,11 +3,13 @@ defmodule RsvpWebWeb.EventController do
   use RsvpWebWeb, :controller
 
   # All of these functions require two parameters, the connection and params sent along from the router
-  def show(conn, params) do
+  def show(conn, %{"id" => id}) do
+    event = Rsvp.EventQueries.get_by_id(id)
+    |> IO.inspect()
     # Render takes three arguments, the conn, the template, and the data to be sent along
-    render(conn, "details.html", data: "Events #{params["id"]}")
+    render(conn, "details.html", event: event)
 
     # Text is a function. It renders out the text supplied as the second argument
-    # text conn, "Events #{params["id"]}"
+    # text conn, "Events #{id}"
   end
 end
--- rsvp_umbrella/apps/rsvp_web/lib/rsvp_web_web/views/event_view.ex
+++ rsvp_umbrella/apps/rsvp_web/lib/rsvp_web_web/views/event_view.ex
@@ -1,4 +1,9 @@
 defmodule RsvpWebWeb.EventView do
   use RsvpWebWeb, :view
 
+  def format_date(date) do
+    {{y, m, d}, _} = NaiveDateTime.to_erl(date)
+    "#{m}/#{d}/#{y}"
+  end
+
 end
